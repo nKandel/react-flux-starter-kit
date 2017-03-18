48cd899ddc1057868f7e30327e00a01ba52ef4df
nKandel <npkand@gmail.com>
2017-03-19 00:01:17 +0545

    creating component introduction

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div className="jumbotron">
+				<h1>Pularsight Administration</h1>
+				<p>React, React Router and Flux</p>
+			</div>
+		);
+	}
+});
+module.exports = Home;
